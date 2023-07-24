508da0df5f42a0b5899ed4255d80fc2993abe8a4
Mohamed Atef <atefhamosa@gmail.com>
2023-07-24 17:12:18 +0300

    Scaffold user & jogging_times models

---
+++ db/migrate/20230724141144_create_jogging_times.rb
@@ -0,0 +1,12 @@
+class CreateJoggingTimes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :jogging_times do |t|
+      t.date :date
+      t.float :distance
+      t.integer :time
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/controllers/jogging_times_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class JoggingTimesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @jogging_time = jogging_times(:one)
+  end
+
+  test "should get index" do
+    get jogging_times_url, as: :json
+    assert_response :success
+  end
+
+  test "should create jogging_time" do
+    assert_difference('JoggingTime.count') do
+      post jogging_times_url, params: { jogging_time: { date: @jogging_time.date, distance: @jogging_time.distance, time: @jogging_time.time, user_id: @jogging_time.user_id } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show jogging_time" do
+    get jogging_time_url(@jogging_time), as: :json
+    assert_response :success
+  end
+
+  test "should update jogging_time" do
+    patch jogging_time_url(@jogging_time), params: { jogging_time: { date: @jogging_time.date, distance: @jogging_time.distance, time: @jogging_time.time, user_id: @jogging_time.user_id } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy jogging_time" do
+    assert_difference('JoggingTime.count', -1) do
+      delete jogging_time_url(@jogging_time), as: :json
+    end
+
+    assert_response 204
+  end
+end
