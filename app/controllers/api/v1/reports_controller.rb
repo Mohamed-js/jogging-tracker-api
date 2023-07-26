c56c8c6a19f4fddc571f91830141cbefdc59087b
Mohamed Atef <atefhamosa@gmail.com>
2023-07-26 06:50:23 +0300

    Style with Rubocop

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -14,11 +14,11 @@ module Api
       private
 
       def calculate_average_speed_and_distance(jogging_times)
-        total_distance = jogging_times.sum(&:distance) # In km 150.6
-        total_time = jogging_times.sum(&:time) # In minutes 100
-        average_speed = total_distance / (total_time / 60.0) # Speed (km/hr) 
+        total_distance = jogging_times.sum(&:distance) # In km
+        total_time = jogging_times.sum(&:time) # In minutes
+        average_speed = total_distance / (total_time / 60.0) # Speed (km/hr) - If we need it in minutes, we delete the /60
 
-        { average_speed: average_speed, total_distance: total_distance, speed_unit: "km/hr" }
+        { average_speed: average_speed, total_distance: total_distance, speed_unit: 'km/hr' }
       end
     end
   end
